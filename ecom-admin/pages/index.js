4ad7f39f4a4e52b0445fca3e089eda751bc50cbb
Gaurav-Singh-G <gaurav13579gsc@gmail.com>
2023-06-27 00:10:21 +0530

    Basic Admin Panel Completed

---
+++ ecom-admin/pages/index.js
@@ -0,0 +1,24 @@
+// import Image from 'next/image'
+// import { Inter } from 'next/font/google'
+// import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout"
+import { useSession } from "next-auth/react"
+// const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  const {data: session} = useSession();
+  if(!session) return;
+  return <Layout>
+    <div className="text-red-900 flex justify-between">
+      <h1>Hello, {session?.user?.name}</h1>
+      <div className="bg-gray-300 flex gap-1 text-black rounded-lg">
+        <img src={session?.user?.image} alt="" className="w-6 h-6"/>
+        <span className="px-2">
+          {session?.user?.name}
+        </span>
+      </div>
+      
+    </div>
+  </Layout>
+}
+ 
